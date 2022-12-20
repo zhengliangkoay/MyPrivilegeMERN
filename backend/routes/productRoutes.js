42cf161750e065498d796def75b39c55dd29637c
HEGoh30 <huayeeih30@gmail.com>
2022-12-20 15:10:32 +0800

    Delete repeated imports

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router()
 import { 
     getProductById,
     getProducts, 
-    createProductReview
+    createProductReview,
+    deleteProduct,
+    updateProduct,
+    createProduct,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
-import { getProductById, getProducts,deleteProduct,updateProduct,createProduct, createProductReview } from '../controllers/productController.js'
+// import { getProductById, getProducts,deleteProduct,updateProduct,createProduct, createProductReview } from '../controllers/productController.js'
 
 router.route('/:id/reviews').post(protect, createProductReview)
 router.route('/').get(getProducts)
